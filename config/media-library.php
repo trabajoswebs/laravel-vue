267429c3c00534e2cdab5cbc1e869bfdb2761f3c
Johan <gonzalezjoan@hotmail.com>
2025-10-11 00:57:35 +0200

    config(media-library): ajusta queue/responsive según nuevo perfil de avatar

--- config/media-library.php
+++ config/media-library.php
@@ -54,12 +54,6 @@ return [
      */
     'use_default_collection_serialization' => false,
 
-    /*
-     * The fully qualified class name of the model used for temporary uploads.
-     *
-     * This model is only used in Media Library Pro (https://medialibrary.pro)
-     */
-    'temporary_upload_model' => Spatie\MediaLibraryPro\Models\TemporaryUpload::class,
 
     /*
      * When enabled, Media Library Pro will only process temporary uploads that were uploaded
